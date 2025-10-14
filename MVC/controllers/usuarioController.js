101907b0449773ee3536051865241ea1f41fde74
Manganao <ens-gabrielbielick@ugv.edu.br>
2025-10-14 20:42:26 -0300

    1.34

--- MVC/controllers/usuarioController.js
+++ MVC/controllers/usuarioController.js
@@ -5,16 +5,7 @@ class UsuarioController {
   static listar(req, res) {
     
     const usuarios = [
-      new Usuario("João Silva", "joao@email.com", "123456"),
-      new Usuario("Maria Oliveira", "maria@email.com", "123456"),
-      new Usuario("Carlos Souza", "carlos@email.com", "123456"),
-      new Usuario("Ana Costa", "ana@email.com", "123456"),
-      new Usuario("Pedro Santos", "pedro@email.com", "123456"),
-      new Usuario("Juliana Lima", "juliana@email.com", "123456"),
-      new Usuario("Felipe Almeida", "felipe@email.com", "123456"),
-      new Usuario("Beatriz Martins", "bia@email.com", "123456"),
-      new Usuario("Lucas Rocha", "lucas@email.com", "123456"),
-      new Usuario("Camila Fernandes", "camila@email.com", "123456"),
+      new Usuario(1, "Alice", "alice@email.com", "123456"),
     ];
 
     res.json(usuarios);
